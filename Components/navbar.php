a5a3740d8926d857b1cdb739a098a964521c90d7
kevinvlad03 <vlad.dumitru1006@stud.fils.upb.ro>
2023-11-22 13:58:10 +0200

    Navbar and Search Bar Initial

---
+++ Components/navbar.php
@@ -0,0 +1,17 @@
+<header>
+
+    <nav>
+        <ul class="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">News</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+    </nav>
+
+    <img class="logo" src="" alt="logo">
+
+    <ul class="nav-links">
+        <li><a href="#"><button class="navbtn">Friends</button></a></li>
+        <li><a href="#"><button class="navbtn">Profile</button></a></li>
+
+</header>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GameHarbor</title>
+    <link href="css/index_style.css" rel="stylesheet">
+    <link href="css/navbar.css" rel="stylesheet">
+    <link href="css/searchbar.css" rel="stylesheet">
+
+    <?php include_once "Components/navbar.php" ?>
+</head>
+<body>
+<div id="cover">
+    <form method="get" action="">
+        <div class="tb">
+            <div class="td"><input type="text" placeholder="Search" required></div>
+            <div class="td" id="s-cover">
+                <button class="searchbtn" type="submit">
+                    <div id="s-circle"></div>
+                    <span></span>
+                </button>
+            </div>
+        </div>
+    </form>
+</div>
+
+</body>
+</html>
